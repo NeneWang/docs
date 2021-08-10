1371e06ca102d37246b5a4f132b59c7ab8e41776
NelsonWang <wangnelson2@gmail.com>
2021-08-09 22:29:01 -0400

    Sections | more sections

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -58,41 +58,70 @@ module.exports = {
         collapsable: false,
         children: [
           '',
+          'Guide',
+          'Plan',
+          'random',
+          
         ]
       }],
       '/web/': [{
         title: 'Web',
         collapsable: false,
         children: [
-          '',
+          'HTML',
+          'Javascript',
+          'CSS',
+          'Sass',
+          'React',
+          'Angular',
+          'Vue',
+          'Next',
+          'MongoDB',
+          'MySQL',
+          'Express',
+          'Laravel',
+          'Django',
+          'PHP',
         ]
       }],
       '/automation/': [{
         title: 'Automation',
         collapsable: false,
         children: [
-          '',
+          'Makefile',
+          'Git',
+          'Shell',
+          'Python',
+          'UIPath',
+          'Java',
         ]
       }],
       '/desktop/': [{
         title: 'Desktop',
         collapsable: false,
         children: [
-          '',
+          'Electron',
         ]
       }],
       '/game/': [{
         title: 'Game',
         collapsable: false,
         children: [
-          '',
+          'Unity',
+          'Engine',
+          'Utilities',
+          'Animation',
+          'Models',
         ]
       }],
       '/mobile/': [{
         title: 'Mobile',
         collapsable: false,
         children: [
-          '',
+          'ReactNative',
+          'Flutter',
+          'XCode',
+          'AndroidStudio',
         ]
       }],
     }
