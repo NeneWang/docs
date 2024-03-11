3a5b4a3558dd98a36bdb85e6e32322f3c4338f5f
NeneWang <wangnelson2@gmail.com>
2024-03-11 04:30:27 +0000

    Deploying to gh-pages from @ NeneWang/code-documentation@f1810f4e508e82d3ff79df15ad1bb0b739ead2f3 🚀

---
+++ assets/backlog.html-BN9NXHkT.js
@@ -0,0 +1,35 @@
+import{_ as i,r as l,o,c,a as n,d as a,w as t,e as p,b as e}from"./app-DfBSGbdE.js";const r={},u={class:"table-of-contents"},d=p(`<h2 id="_1-introduction" tabindex="-1"><a class="header-anchor" href="#_1-introduction"><span>1 Introduction</span></a></h2><ol><li>Notes for Mantainance of this content.</li><li>Here are more of the general backlog of features to add into the page. As marked will be the different Stages</li></ol><ul><li>[x] Writting the base Raw Tips (25) &lt;-- Current</li><li>[ ] Developing the base Raw Tips (25) <ul><li>[ ] Creating Tools for the base Raw Tips (25)</li><li>[ ] Or also test my ideas in simulations + research. (25)</li></ul></li><li>[ ] Writing it inot a details Blog (25)</li></ul><h2 id="format" tabindex="-1"><a class="header-anchor" href="#format"><span>Format</span></a></h2><div class="language-markdown line-numbers-mode" data-ext="md" data-title="md"><pre class="language-markdown"><code>
+<span class="token title important"><span class="token punctuation">##</span> [number] [Title]</span>
+
+<span class="token italic"><span class="token punctuation">*</span><span class="token content">Important to enumerate the title section. So that it can be referenced on the large scope.</span><span class="token punctuation">*</span></span>
+
+<span class="token italic"><span class="token punctuation">*</span><span class="token content">Important to list the rules or tips with numbers, so that it can be referenced quickly on the projects.</span><span class="token punctuation">*</span></span>
+
+<span class="token list punctuation">1.</span> Rule 1
+<span class="token list punctuation">2.</span> Rule 2
+<span class="token list punctuation">3.</span> Rule 3
+
+
+
+<span class="token title important"><span class="token punctuation">###</span> Stats</span>
+
+<span class="token list punctuation">-</span> <span class="token bold"><span class="token punctuation">**</span><span class="token content">Priority</span><span class="token punctuation">**</span></span>: [priority]
+<span class="token list punctuation">-</span> <span class="token bold"><span class="token punctuation">**</span><span class="token content">Difficulty</span><span class="token punctuation">**</span></span>: [difficulty]
+<span class="token list punctuation">-</span> <span class="token bold"><span class="token punctuation">**</span><span class="token content">Time per week</span><span class="token punctuation">**</span></span>: [time] 
+
+<span class="token bold"><span class="token punctuation">**</span><span class="token content"><span class="token italic"><span class="token punctuation">*</span><span class="token content">Definitions</span><span class="token punctuation">*</span></span></span><span class="token punctuation">**</span></span>
+
+<span class="token italic"><span class="token punctuation">*</span><span class="token content">Priority</span><span class="token punctuation">*</span></span>: The prioriity after considerations of the costs (difficulty and time)
+<span class="token italic"><span class="token punctuation">*</span><span class="token content">Time Per Week</span><span class="token punctuation">*</span></span>: The cost in hours per weeks.
+
+<span class="token title important"><span class="token punctuation">###</span> [date] </span>
+
+[feedback]
+
+<span class="token title important"><span class="token punctuation">###</span> Project</span>
+
+<span class="token italic"><span class="token punctuation">*</span><span class="token content">This section can go through various project and features ideas to support and automate this strategy. And that project will go through different stages: Ideation, Prototype, Internal Testing, Beta, Production which should be makrked in order to continue developing on itself.</span><span class="token punctuation">*</span></span>
+
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="tags" tabindex="-1"><a class="header-anchor" href="#tags"><span>Tags</span></a></h2><p>The following tags might be applied.</p>`,7);function m(v,k){const s=l("router-link");return o(),c("div",null,[n("nav",u,[n("ul",null,[n("li",null,[a(s,{to:"#_1-introduction"},{default:t(()=>[e("1 Introduction")]),_:1})]),n("li",null,[a(s,{to:"#format"},{default:t(()=>[e("Format")]),_:1})]),n("li",null,[a(s,{to:"#tags"},{default:t(()=>[e("Tags")]),_:1})])])]),d])}const b=i(r,[["render",m],["__file","backlog.html.vue"]]),g=JSON.parse('{"path":"/posts/guide/backlog.html","title":"Backlog","lang":"en-US","frontmatter":{"title":"Backlog"},"headers":[{"level":2,"title":"1 Introduction","slug":"_1-introduction","link":"#_1-introduction","children":[]},{"level":2,"title":"Format","slug":"format","link":"#format","children":[]},{"level":2,"title":"Tags","slug":"tags","link":"#tags","children":[]}],"git":{"updatedTime":1710098392000,"contributors":[{"name":"NelsonWang","email":"wangnelson2@gmail.com","commits":1}]},"filePathRelative":"posts/guide/backlog.md","excerpt":"\\n<h2>1 Introduction</h2>\\n<ol>\\n<li>Notes for Mantainance of this content.</li>\\n<li>Here are more of the general backlog of features to add into the page. As marked will be the different Stages</li>\\n</ol>\\n<ul>\\n<li>[x] Writting the base Raw Tips (25) &lt;-- Current</li>\\n<li>[ ] Developing the base Raw Tips (25)\\n<ul>\\n<li>[ ] Creating Tools for the base Raw Tips (25)</li>\\n<li>[ ] Or also test my ideas in simulations + research. (25)</li>\\n</ul>\\n</li>\\n<li>[ ] Writing it inot a details Blog (25)</li>\\n</ul>"}');export{b as comp,g as data};
